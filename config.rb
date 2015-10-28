fb16d7f519e70cec94f105c60c5290cf657143ea
Taras Bilous <tbilous@yahoo.com>
2015-10-28 20:27:41 +0200

    section - warning

--- config.rb
+++ config.rb
@@ -10,23 +10,7 @@ sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "scripts"
 sourcemap     = true
-output_style  = :compressed
+#output_style  = :compressed
 sass_options  = { cache: false }
 line_comments = false
 
-
-#on_stylesheet_saved do |file|
-#  css = File.read(file)
-#  map = file + '.map'
-
-#  if File.exists? map
-#    result = AutoprefixerRails.process(css,
-#      from: file,
-#      to:   file,
-#      map:  { prev: File.read(map), inline: false })
-#    File.open(file, 'w') { |io| io << result.css }
-#    File.open(map,  'w') { |io| io << result.map }
-#  else
-#    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
-#  end
-#end
